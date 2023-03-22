39e05f5950194d913241db7e7bda5cf230e060de
Alexander Grooff <alexandergrooff@gmail.com>
2023-03-22 17:48:10 +0100

    feat: parse arrow label

--- cmd/parse.go
+++ cmd/parse.go
@@ -3,11 +3,55 @@ package cmd
 import (
 	"errors"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/elliotchance/orderedmap/v2"
 	log "github.com/sirupsen/logrus"
 )
+
+func setArrowWithLabel(matchedLine []string, data *orderedmap.OrderedMap[string, []string]) {
+	parent := matchedLine[0]
+	label := matchedLine[1]
+	child := matchedLine[2]
+	log.Debug("Setting arrow from ", parent, " to ", child, " with label ", label)
+	// Check if the parent is in the map
+	if children, ok := data.Get(parent); ok {
+		// If it is, append the child to the list of children
+		data.Set(parent, append(children, child))
+	} else {
+		// If it isn't, add it to the map
+		data.Set(parent, []string{child})
+	}
+	// Check if the child is in the map
+	if _, ok := data.Get(child); ok {
+		// If it is, do nothing
+	} else {
+		// If it isn't, add it to the map
+		data.Set(child, []string{})
+	}
+}
+
+func setArrow(matchedLine []string, data *orderedmap.OrderedMap[string, []string]) {
+	parent := matchedLine[0]
+	child := matchedLine[1]
+	log.Debug("Setting arrow from ", parent, " to ", child)
+	// Check if the parent is in the map
+	if children, ok := data.Get(parent); ok {
+		// If it is, append the child to the list of children
+		data.Set(parent, append(children, child))
+	} else {
+		// If it isn't, add it to the map
+		data.Set(parent, []string{child})
+	}
+	// Check if the child is in the map
+	if _, ok := data.Get(child); ok {
+		// If it is, do nothing
+	} else {
+		// If it isn't, add it to the map
+		data.Set(child, []string{})
+	}
+}
 
 func mermaidFileToMap(mermaidFile string) (*orderedmap.OrderedMap[string, []string], error) {
 	// Parse the mermaid code into a map
@@ -27,6 +71,16 @@ func mermaidFileToMap(mermaidFile string) (*orderedmap.OrderedMap[string, []stri
 	data := orderedmap.NewOrderedMap[string, []string]()
 	// Split the mermaid code into lines
 	lines := strings.Split(string(mermaid), "\n")
+
+	arrowPattern, err := regexp.Compile(`^(.+)\s+-->\s+(.+)$`)
+	if err != nil {
+		return nil, err
+	}
+	arrowWithLabelPattern, err := regexp.Compile(`^(.+)\s+-->\|(.+)\|\s+(.+)$`)
+	if err != nil {
+		return nil, err
+	}
+
 	// Iterate over the lines
 	log.Debug("Parsing mermaid code from ", mermaidFile)
 	for _, line := range lines {
@@ -35,32 +89,12 @@ func mermaidFileToMap(mermaidFile string) (*orderedmap.OrderedMap[string, []stri
 			continue
 		}
 		log.Debug("Parsing line: ", line)
-		// Split the line into words
-		words := strings.Split(line, " ")
-		// The first word is the parent
-		parent := words[0]
-		// The second word is the arrow
-		arrow := words[1]
-		// The third word is the child
-		child := words[2]
-		// Check that the arrow is "-->"
-		if arrow != "-->" {
-			return nil, errors.New("Invalid arrow")
-		}
-		// Check if the parent is in the map
-		if children, ok := data.Get(parent); ok {
-			// If it is, append the child to the list of children
-			data.Set(parent, append(children, child))
+		if match := arrowWithLabelPattern.FindStringSubmatch(line); match != nil {
+			setArrowWithLabel(match[1:], data)
+		} else if match := arrowPattern.FindStringSubmatch(line); match != nil {
+			setArrow(match[1:], data)
 		} else {
-			// If it isn't, add it to the map
-			data.Set(parent, []string{child})
-		}
-		// Check if the child is in the map
-		if _, ok := data.Get(child); ok {
-			// If it is, do nothing
-		} else {
-			// If it isn't, add it to the map
-			data.Set(child, []string{})
+			return nil, errors.New("Could not parse line: " + line)
 		}
 	}
 	return data, nil
