27fc307ffe7d0d83015ca25a1a02b51d56dbd0f5
Alexander Grooff <alexandergrooff@gmail.com>
2024-09-25 22:59:49 +0200

    auto generate

--- cmd/web.go
+++ cmd/web.go
@@ -28,7 +28,6 @@ func setupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		})
