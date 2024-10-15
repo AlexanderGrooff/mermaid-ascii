a4cceaf3c847f6f825bbb7fae73f5bc2763d0816
zdcthomas <zdcthomas@yahoo.com>
2024-10-15 12:17:28 -0400

    update nix go mod sha and add flake checker action

--- flake.nix
+++ flake.nix
@@ -3,17 +3,21 @@
     alex.url = "github:AlexanderGrooff/nix-flakes";
   };
 
-  outputs = { self, flake-utils, nixpkgs, alex }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    alex,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         devShells.default = alex.devShells.${system}.go;
         packages.default = pkgs.buildGoModule {
-            name = "mermaid-ascii";
-            src = ./.;
-            vendorHash = "sha256-u2dsmqGoezsu/jSQBv4UxdznTW8B84PeM+FB4n6abUQ=";
+          name = "mermaid-ascii";
+          src = ./.;
+          vendorHash = " sha256-Lnksoj7vx3T1lJfJRuGJHqKMvwra7t7TGn8NwjEkMMI=";
         };
       }
     );
